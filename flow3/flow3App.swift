f5d61c28d426012e270cdf4a8c7dcafdb7e81813
asayel <example.com>
2022-12-12 08:45:26 +0300

    Initial Commit

---
+++ flow3/flow3App.swift
@@ -0,0 +1,17 @@
+//
+//  flow3App.swift
+//  flow3
+//
+//  Created by asayel on 18/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct flow3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
