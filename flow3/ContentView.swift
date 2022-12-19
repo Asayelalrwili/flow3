6b7237a0b48634d7a6bc918ad07c74ac447017d2
asayel <example.com>
2022-12-19 11:05:31 +0300

    add login pages

--- flow3/ContentView.swift
+++ flow3/ContentView.swift
@@ -1,25 +1,179 @@
 //
-//  ContentView.swift
-//  flow3
+//  Login.swift
+//  flow2
 //
-//  Created by asayel on 18/05/1444 AH.
+//  Created by asayel on 14/05/1444 AH.
 //
 
 import SwiftUI
+import LocalAuthentication
+import AuthenticationServices
 
 struct ContentView: View {
+    
+    @State var username = ""
+    @State var Password = ""
+    @State var unlocked = false
+    @State var text = ""
+    @State var showinghomescreen = false
+    @State var showingsignupscreen = false
+    @State var showingIDscreen = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack{
+            
+            ZStack {
+            
+           // Image("green")
+                //.padding(.trailing,170)
+                //.padding(.bottom,690)
+            Image("3")
+                .resizable()
+                .frame(width:200, height: 200)
+                .padding(.top,617.0)
+                .padding(.leading,183)
+            
+            
+            Text("حياك الله معنا")
+                .font(.title)
+                .fontWeight(.regular)
+                .padding()
+                .padding(.leading,175)
+                .foregroundColor(Color("p"))
+                .padding(.bottom,500)
+            
+                
+                
+            Text("اللقب :")
+                .font(.body)
+                .fontWeight(.regular)
+                .padding()
+                .padding(.leading,250)
+                .foregroundColor(Color("p"))
+                .padding(.bottom,270)
+                .padding()
+            
+            Rectangle()
+                .fill(Color("p"))
+                .frame(width: 316, height: 2)
+                .cornerRadius(23.5)
+                .padding(.bottom,160)
+                
+                
+                TextField("" , text: self.$username)
+                    .multilineTextAlignment(.trailing)
+                    .padding(.trailing,50)
+                    .padding(.bottom,200)
+            
+            VStack{
+                ZStack{
+                    Text("كلمة السر :")
+                        .font(.body)
+                        .fontWeight(.regular)
+                        .padding()
+                        .padding(.leading,250)
+                        .foregroundColor(Color("p"))
+                        .padding(.bottom,100)
+                        .padding()
+                    
+                    Rectangle()
+                        .fill(Color("p"))
+
+                        .frame(width: 316, height: 2)
+                        .cornerRadius(23.5)
+                        .padding(.top,15)
+                                            
+                    SecureField("" , text: $Password)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.trailing)
+                        .padding(.trailing,50)
+                        .padding(.bottom,30)
+                    
+                    Text("ما عندك حساب ؟")
+                        .foregroundColor(.gray)
+                        .padding(.top,250)
+                        .padding(.leading,130)
+                    
+                    Button(" سجل الان  ") {
+                        showingsignupscreen = true
+                    } .foregroundColor(Color("p"))
+                        .navigationDestination(isPresented: $showingsignupscreen) {
+                            Signup()
+                            
+                        }.padding(.top,250)
+                        .padding(.trailing,85)
+                    
+                  
+                    
+                }
+            }
+                    
+                
+                
+                Button("دخول") {
+                    showinghomescreen = true
+                } .foregroundColor(Color("p"))
+                    .frame(width: 316, height: 47) .background (Color("g"))  .cornerRadius(23.5)
+                    .navigationDestination(isPresented: $showinghomescreen) {
+                        EmptyView()
+                            //.navigationBarBackButtonHidden()
+                    }.padding(.top,150)
+                
+                
+                
+               // Text(text)
+                  //  .bold()
+              //  Button("Sign in with Apple Face ID") { authenticate()
+                    
+               // }.foregroundColor(.white)
+                   // .frame(width: 316, height: 47)
+                  //  .background (Color.black)
+                  ///  .cornerRadius(23.5)
+                  //  .navigationDestination//(isPresented: $showinghomescreen) {
+                      //  EmptyView()
+                          //  .navigationBarBackButtonHidden()
+                  //  }.padding(.top,300)
+                
+                
+              
+            
         }
-        .padding()
+        
+            
+        }
     }
+    func authenticate() {
+        
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "This is for security reasons") { success, authenticationError in
+                
+                
+                if success {
+                    text = ""
+                    showinghomescreen = true
+                }
+                else { text = "There was a problem!"
+                    
+                }
+                
+            }
+        } else {
+            text = "Phone does not have Biometrics"
+                
+            
+        }
+    }
+    
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Content_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
---
+++ flow3/OnBordaing.swift
@@ -0,0 +1,134 @@
+//
+//  OnBoarding.swift
+//  Ivy
+//
+//  Created by Alaa Alabdullah on 14/12/2022.
+//
+
+import SwiftUI
+
+enum OnbordingType: CaseIterable {
+    case scan
+    case track
+    case remind
+    
+    
+    
+    var image: String {
+        switch self {
+        case .scan:
+            return "read"
+        case .track:
+            return "help"
+        case .remind:
+            return "clear"
+            
+        }
+    }
+    
+    
+    var description: String {
+        switch self {
+        case .scan:
+            return "انشطه يوميه تعزز السعادة وتحرر عقلك من الافكار السلبيه."
+        case .track:
+            return "شارك كل مشاعرك بثقه وصراحه فكلنا داعمون على نفس الطريق."
+        case .remind:
+            return "مستعد تتخطى و تكون لنفسك ولمجتمعك داعم ؟"
+            
+            
+        }
+        
+    }
+}
+
+struct OnbordingView: View {
+    
+    var selectedOnbordingType: OnbordingType = .scan
+    
+    
+    var body: some View {
+        
+        ZStack {
+            
+               
+            
+            
+            
+                TabView {
+                    
+                    ForEach(OnbordingType.allCases, id: \.description) { onbording in
+                        SingleOnbording(onbordingType: onbording)
+                    }
+                    
+                    
+                }
+                .tabViewStyle(.page(indexDisplayMode: .always))
+                .indexViewStyle(.page(backgroundDisplayMode: .always))
+            
+        }
+        
+        
+    }
+    
+    
+}
+
+struct OnbordingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnbordingView()
+    }
+}
+
+struct SingleOnbording: View {
+    
+    let onbordingType: OnbordingType
+    
+    @State private var showLogingPage = false
+    @State private var showTabViewPage = false
+    
+    var body: some View {
+        
+        
+            VStack {
+                
+                Button("skip"){
+                    showTabViewPage.toggle()
+                }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing ,30)
+                    .foregroundColor(.secondary)
+                    .fullScreenCover(isPresented: $showTabViewPage) {
+                        EmptyView()
+                    }
+                
+                Spacer()
+            
+                Image(onbordingType.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                
+                Text(onbordingType.description)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                
+                if onbordingType == .remind {
+                    
+                       // SignFace()
+                        //SignupID()
+                        
+                    
+                }
+               Spacer()
+                
+                
+            }
+            .padding(.horizontal,40)
+            
+        
+        }
+        
+
+        
+    }
